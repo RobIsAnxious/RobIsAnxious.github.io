4d2dde28c277beeff9a0f56794ccb349b93de303
RobIsAnxious <regfoxes@gmail.com>
2015-02-20 01:12:21 -0700

    separated css and javascript into new files.

---
+++ animatesound.js
@@ -0,0 +1,75 @@
+
+//Animations
+$('#red').click(function () {
+    $(this).animate({
+        'width': '70px',
+        'height': '70px'
+    }, 200);
+
+    $(this).animate({
+        'width': '100px',
+        'height': '100px'
+    }, 200);
+
+});
+
+$('#blue').click(function () {
+    $(this).animate({
+        'width': '70px',
+        'height': '70px'
+    }, 200);
+    $(this).animate({
+        'width': '100px',
+        'height': '100px'
+    }, 200);
+
+});
+
+$('#green').click(function () {
+    $(this).animate({
+        'width': '70px',
+        'height': '70px'
+    }, 200);
+    $(this).animate({
+        'width': '100px',
+        'height': '100px'
+    }, 200);
+
+});
+
+$('#yellow').click(function () {
+    $(this).animate({
+        'width': '70px',
+        'height': '70px'
+    }, 200);
+    $(this).animate({
+        'width': '100px',
+        'height': '100px'
+    }, 200);
+
+});
+
+//Sound playing
+var ass = $('#myAss')[0];
+$('#red').click(function(){
+	ass.currentTime=0;
+    ass.play();
+});
+
+var dicks = $('#myDicks')[0];
+$('#blue').click(function(){
+    dicks.currentTime=0;
+	dicks.play();
+});
+
+var peen = $('#myPeen')[0];
+$('#green').click(function(){
+    peen.currentTime=0;
+	peen.play();
+});
+
+var piss = $('#myPiss')[0];
+$('#yellow').click(function(){
+    piss.currentTime=0;
+	piss.play();
+});
